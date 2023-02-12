353567ac750f318bf1a855fe690dde138cec823e
MR-Addict <2750417853@qq.com>
2023-02-12 16:55:23 +0800

    shorten redis expire time

--- src/api/log.ts
+++ src/api/log.ts
@@ -20,7 +20,7 @@ router.get("/", async (req: Request, res: Response) => {
     const lines = Number(req.query.lines) || 1;
     const logData = await readLastLines.read(file, lines);
 
-    await redisClient.set(logFile, logData, { EX: 60 * 60, NX: true });
+    await redisClient.set(logFile, logData, { EX: 60 * 10, NX: true });
     return res.status(200).send(logData);
   } catch (error) {
     console.error(error);
