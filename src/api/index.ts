f893bbd13305afe1cd98379532d224b2e34dfe52
MR-Addict <2750417853@qq.com>
2023-02-15 00:49:42 +0800

    fix cache control

--- src/api/index.ts
+++ src/api/index.ts
@@ -6,6 +6,6 @@ import log from "./log";
 const api = express();
 
 api.use("/log", log);
-api.use(express.static(path.join(process.cwd(), "public"), { maxAge: 0 }));
+api.use(express.static(path.join(process.cwd(), "public"), { maxAge: 1000 * 60 * 10 }));
 
 export default api;
