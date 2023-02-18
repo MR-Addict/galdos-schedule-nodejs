59edb72037ebac6a26eb8900d9257db92920cab8
MR-Addict <2750417853@qq.com>
2023-02-19 03:37:22 +0800

    turn off cache on localhost

--- src/www/index.ts
+++ src/www/index.ts
@@ -4,9 +4,10 @@ import express from "express";
 import views from "./views";
 
 const app = express();
+const isLocalhost = process.env.ISLOCALHOST === "TRUE";
 
 app.use("/", views);
-app.use(express.static(path.join(process.cwd(), "public"), { maxAge: 1000 * 60 * 60 * 4 }));
+app.use(express.static(path.join(process.cwd(), "public"), { maxAge: isLocalhost ? 0 : 1000 * 60 * 60 * 4 }));
 
 app.set("view engine", "pug");
 app.set("views", path.join(process.cwd(), "src/www/views"));
