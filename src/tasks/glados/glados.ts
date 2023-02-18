59d9d979275b336fce29463fbad3d0003c7ae9a5
MR-Addict <2750417853@qq.com>
2023-02-18 16:47:07 +0800

    fix tasks

--- src/tasks/glados/glados.ts
+++ src/tasks/glados/glados.ts
@@ -1,8 +1,9 @@
+import path from "path";
+
 import checkin from "./checkin";
 import { logPath } from "@/config";
 import { writeLog } from "@/lib/log";
 import { sendEmail } from "@/lib/report";
-import path from "path";
 
 export default async function glados(report: "fail" | "success" | "both" = "fail") {
   const result = await checkin();
@@ -10,9 +11,9 @@ export default async function glados(report: "fail" | "success" | "both" = "fail
   const filePath = path.join(logPath, "glados.csv");
 
   console.log(result.message);
-  await writeLog(filePath, { date, message: result.message });
+  writeLog(filePath, { date, message: result.message });
 
   if (report === "both" || (report === "success" && result.status) || (report === "fail" && !result.status)) {
-    await sendEmail({ to: "MR-Addict@qq.com", subject: "Glados Checkin", text: result.message });
+    sendEmail({ to: "MR-Addict@qq.com", subject: "Glados Checkin", text: result.message });
   }
 }
