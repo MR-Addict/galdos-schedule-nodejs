300d8142ace1f5b3300ffc8d513c4a3c539cfd2d
MR-Addict <2750417853@qq.com>
2023-02-19 03:46:04 +0800

    update body

--- public/index.js
+++ public/index.js
@@ -1,9 +1,9 @@
 function toggle(currentTab) {
   if (document.querySelector(`button.active[tab-name="${currentTab}"]`)) return;
-  Array.from(document.querySelectorAll("header button")).forEach((item) => {
+  Array.from(document.querySelectorAll("button")).forEach((item) => {
     const tabName = item.querySelector("span:first-of-type").innerHTML;
     const tabButton = document.querySelector(`button[tab-name="${tabName}"]`);
-    const tabSection = document.querySelector(`section[tab-name="${tabName}"]`);
+    const tabSection = document.querySelector(`ul[tab-name="${tabName}"]`);
     tabButton.classList.toggle("active");
     tabSection.classList.toggle("active");
   });
