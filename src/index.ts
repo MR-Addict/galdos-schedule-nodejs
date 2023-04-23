47cefcf3bd45c0c049cedcdd0595b6baa01ccc5c
MR-Addict <2750417853@qq.com>
2023-04-23 15:02:45 +0800

    update tick schedule time

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import www from "@/www";
 import { glados, tick } from "@/tasks";
 import { registerTask } from "@/lib/utils";
 
-registerTask("tick-tick-tick", "0 0 8 * * *", async () => await tick());
+registerTask("tick-tick-tick", "0 0 6 * * *", async () => await tick());
 registerTask("glados-checkin", "0 0 6 * * *", async () => await glados());
 
 www.listen(3000, () => console.log("Listening on http://localhost:3000"));
